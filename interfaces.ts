5bc57a290880fe3430fe383778d8a6a156485948
Diwas Lamichhane <diwaslamichhane11@gmail.com>
2025-05-06 09:09:08 +0545

    Interface (Defining Object Shapes)

---
+++ interfaces.ts
@@ -0,0 +1,14 @@
+// Used mainly to define object shapes
+// Example
+
+interface ObjInterface{
+    name: string;
+    age: number;
+    email: string;
+    gender: string;
+}
+
+function abcd(obj: ObjInterface){
+
+}
+abcd({name: "Diwas", age: 22, email: "diwas@gmail.com", gender: "male"});
