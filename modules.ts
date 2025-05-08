3fafdab293c2c70060e130faa88cd487b523fbc5
Diwas Lamichhane <diwaslamichhane11@gmail.com>
2025-05-08 21:35:39 +0545

    modules (export, import)

---
+++ modules.ts
@@ -0,0 +1,8 @@
+// Module is any file that has an import or export.
+
+
+// I have a file called exportingFile_modules.ts that exports a function called add.
+
+// Importing the add function from exportingFile_modules.ts and performing an addition operation.
+import { add } from './exportingFile_modules.js';
+console.log(add(2, 3)); // 5
