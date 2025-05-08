49bde5734fe25259c66cd816a6beea0eafb88e64
Diwas Lamichhane <diwaslamichhane11@gmail.com>
2025-05-08 21:36:10 +0545

    modules (export, import)

---
+++ exportingFile_modules.ts
@@ -0,0 +1,7 @@
+
+// Operation to add two numbers and exporting the function
+// Check modules.ts for the import statement
+
+export function add(a: number, b: number): number {
+    return a + b;
+  }
